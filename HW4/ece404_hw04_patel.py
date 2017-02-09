de77d1edb197bfb9a522d989d72ce05125d8b0bd
patel344 <Parth@pal-nat186-77-3.itap.purdue.edu>
2017-02-09 10:13:54 -0500

    Adding HW4

--- HW4/ece404_hw04_patel.py
+++ HW4/ece404_hw04_patel.py
@@ -4,29 +4,30 @@ __author__ = 'Parth'
 # Homework Number: 3
 # Name: Parth Patel
 # ECN Login: patel344
-# Due Date: February 2, 2017
+# Due Date: February 9, 2017
 
 from BitVector import BitVector
+import copy
+
 AES_modulus = BitVector(bitstring='100011011')
 
-subBytesTable = []                                                  # for encryption
-invSubBytesTable = []                                               # for decryption
-
+subBytesTable = []
+invSubBytesTable = []
 
 def genTables():
     c = BitVector(bitstring='01100011')
     d = BitVector(bitstring='00000101')
     for i in range(0, 256):
         # For the encryption SBox
-        a = BitVector(intVal = i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
+        a = BitVector(intVal=i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
         # For bit scrambling for the encryption SBox entries:
-        a1,a2,a3,a4 = [a.deep_copy() for x in range(4)]
+        a1, a2, a3, a4 = [a.deep_copy() for x in range(4)]
         a ^= (a1 >> 4) ^ (a2 >> 5) ^ (a3 >> 6) ^ (a4 >> 7) ^ c
         subBytesTable.append(int(a))
         # For the decryption Sbox:
-        b = BitVector(intVal = i, size=8)
+        b = BitVector(intVal=i, size=8)
         # For bit scrambling for the decryption SBox entries:
-        b1,b2,b3 = [b.deep_copy() for x in range(3)]
+        b1, b2, b3 = [b.deep_copy() for x in range(3)]
         b = (b1 >> 2) ^ (b2 >> 5) ^ (b3 >> 7) ^ d
         check = b.gf_MI(AES_modulus, 8)
         b = check if isinstance(check, BitVector) else 0
@@ -51,8 +52,8 @@ def gen_subbytes_table():
     subBytesTable = []
     c = BitVector(bitstring='01100011')
     for i in range(0, 256):
-        a = BitVector(intVal = i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
-        a1,a2,a3,a4 = [a.deep_copy() for x in range(4)]
+        a = BitVector(intVal=i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
+        a1, a2, a3, a4 = [a.deep_copy() for x in range(4)]
         a ^= (a1 >> 4) ^ (a2 >> 5) ^ (a3 >> 6) ^ (a4 >> 7) ^ c
         subBytesTable.append(int(a))
     return subBytesTable
@@ -64,11 +65,11 @@ def gee(keyword, round_constant, byte_sub_table):
     '''
     rotated_word = keyword.deep_copy()
     rotated_word << 8
-    newword = BitVector(size = 0)
+    newword = BitVector(size=0)
     for i in range(4):
-        newword += BitVector(intVal = byte_sub_table[rotated_word[8*i:8*i+8].intValue()], size = 8)
+        newword += BitVector(intVal=byte_sub_table[rotated_word[8 * i:8 * i + 8].intValue()], size=8)
     newword[:8] ^= round_constant
-    round_constant = round_constant.gf_multiply_modular(BitVector(intVal = 0x02), AES_modulus, 8)
+    round_constant = round_constant.gf_multiply_modular(BitVector(intVal=0x02), AES_modulus, 8)
     return newword, round_constant
 
 
@@ -79,77 +80,189 @@ def gen_key_schedule_128(key_bv):
     #  Subsequently, each of the 10 rounds uses 4 keywords from the key schedule. We will
     #  store all 44 keywords in the following list:
     key_words = [None for i in range(44)]
-    round_constant = BitVector(intVal = 0x01, size=8)
+    round_constant = BitVector(intVal=0x01, size=8)
     for i in range(4):
-        key_words[i] = key_bv[i*32 : i*32 + 32]
-    for i in range(4,44):
-        if i%4 == 0:
-            kwd, round_constant = gee(key_words[i-1], round_constant, byte_sub_table)
-            key_words[i] = key_words[i-4] ^ kwd
+        key_words[i] = key_bv[i * 32: i * 32 + 32]
+    for i in range(4, 44):
+        if i % 4 == 0:
+            kwd, round_constant = gee(key_words[i - 1], round_constant, byte_sub_table)
+            key_words[i] = key_words[i - 4] ^ kwd
         else:
-            key_words[i] = key_words[i-4] ^ key_words[i-1]
+            key_words[i] = key_words[i - 4] ^ key_words[i - 1]
     return key_words
 
 
 def encrypt(inputfile, outputfile):
+    f = open('hexfile.txt', 'a')
     key = get_encryption_key()
     key_words = gen_key_schedule_128(key)
-    subBytesTable,_ = genTables()
+    subBytesTable, _ = genTables()
     bv = BitVector(filename=inputfile)
-    statearray = [[0 for x in range(4)] for x range(4)]
+    statearray = [[0 for x in range(4)] for x in range(4)]
     FILEOUT = open(outputfile, 'ab')
+    temp_shift = [0]* 4
+
     while bv.more_to_read:
         bitvec = bv.read_bits_from_file(128)
         if len(bitvec) > 0:
             if len(bitvec) != 128:
-                bitvec.pad_from_right(128-len(bitvec))
+                bitvec.pad_from_right(128 - len(bitvec))
 
-            # Filling in statearray and XORing
+            #Filling in statearray and XORing
             for i in range(4):
                 for j in range(4):
-                    statearray[i][j] = bitvec[32*i + 8*j:32*i + 8*(j+1)]
-                    statearray[i][j] ^= key_words[i][8*j:8+(8*j)]
+                    statearray[i][j] = bitvec[32 * i + 8 * j:32 * i + 8 * (j + 1)]
+                    statearray[i][j] ^= key_words[i][8 * j:8 + (8 * j)]
 
-            # Round processing
             for round in range(10):
 
                 # SubBytes
                 for i in range(4):
                     for j in range(4):
-                        statearray[i][j] = subBytesTable[statearray[i][j]]
+                        statearray[i][j] = BitVector(intVal=subBytesTable[int(statearray[i][j])])
 
                 # ShiftRows
-                for i in range(4):
-                    statearray[i] = statearray [i] << i
+                for i in range(1,4):
+                    for j in range(0,4):
+                        temp_shift[(j-i)%4] = statearray[j][i]
+                    for j in range (0,4):
+                        statearray[j][i] = temp_shift[j]
 
                 # ColumnMixing
-                one_time    = BitVector(bitstring='00000001')
-                two_times   = BitVector(bitstring='00000010')
-                three_times = BitVector(bitstring='00000011')
-                col_mix = [two_times, three_times, one_time, one_time]
+                if round != 9:
+                    two_times = BitVector(bitstring='00000010')
+                    three_times = BitVector(bitstring='00000011')
+                    for i in range(4):
+                        temp = (two_times.gf_multiply_modular(statearray[i][0], AES_modulus, 8)) ^ \
+                                           (three_times.gf_multiply_modular(statearray[i][1], AES_modulus, 8)) ^ \
+                                           statearray[i][2] ^ \
+                                           statearray[i][3]
+
+                        temp1 = (two_times.gf_multiply_modular(statearray[i][1], AES_modulus, 8)) ^ \
+                                           (three_times.gf_multiply_modular(statearray[i][2], AES_modulus, 8)) ^ \
+                                           statearray[i][3] ^ \
+                                           statearray[i][0]
+
+                        temp2 = (two_times.gf_multiply_modular(statearray[i][2], AES_modulus, 8)) ^ \
+                                           (three_times.gf_multiply_modular(statearray[i][3], AES_modulus, 8)) ^ \
+                                           statearray[i][0] ^ \
+                                           statearray[i][1]
+
+                        temp3 = (two_times.gf_multiply_modular(statearray[i][3], AES_modulus, 8)) ^ \
+                                           (three_times.gf_multiply_modular(statearray[i][0], AES_modulus, 8)) ^ \
+                                           statearray[i][1] ^ \
+                                           statearray[i][2]
+
+                        statearray[i][0] = temp
+                        statearray[i][1] = temp1
+                        statearray[i][2] = temp2
+                        statearray[i][3] = temp3
+
+
+                    
+                # Add Round Key
                 for i in range(4):
                     for j in range(4):
-                        statearray[i][j] = statearray[i][j].gf_multiply_modular(col_mix[i], AES_modulus, 8)
-                    col_mix = [col_mix[-1], col_mix[0], col_mix[1], col_mix[3]]
-
-                # Add Round Key
-                    for i in range(4):
-                        for j in range(4):
-                            statearray[i][j] ^= key_words[round+1][8*j:8+(8*j)]
+                        statearray[i][j] ^= key_words[(4 * (round + 1)) + i][8 * j:8 + (8 * j)]
 
             for i in range(4):
                 for j in range(4):
                     statearray[i][j].write_to_file(FILEOUT)
+                    f.write(statearray[i][j].get_bitvector_in_hex())
+    f.close()
     FILEOUT.close()
     return
 
-def decrypt():
-    pass
 
+def decrypt(inputfile, outputfile):
+    f = open('hexdecrypt.txt', 'a')
+    temp_shift = [0]*4
+    key = get_encryption_key()
+    key_words = gen_key_schedule_128(key)
+    _, invSubBytesTable = genTables()
+    bv = BitVector(filename=inputfile)
+    statearray = [[0 for x in range(4)] for x in range(4)]
+    FILEOUT = open(outputfile, 'ab')
+    while bv.more_to_read:
+        bitvec = bv.read_bits_from_file(128)
+        if len(bitvec) > 0:
+            if len(bitvec) != 128:
+                bitvec.pad_from_right(128 - len(bitvec))
+
+            # Filling in statearray and XORing
+            for i in range(4):
+                for j in range(4):
+                    statearray[i][j] = bitvec[32 * i + 8 * j:32 * i + 8 * (j + 1)]
+                    statearray[i][j] ^= key_words[-(4-i)][8 * j:8 + (8 * j)]
+
+
+            for round in range(10,0,-1):
+                # Inverse ShiftRows
+                for i in range(1,4):
+                    for j in range(0,4):
+                        temp_shift[(j+i)%4] = statearray[j][i]
+                    for j in range (0,4):
+                        statearray[j][i] = temp_shift[j]
+                    
+                # Inverse SubBytes
+                for i in range(4):
+                    for j in range(4):
+                        statearray[i][j] = BitVector(intVal=invSubBytesTable[int(statearray[i][j])])
+
+                # Add Round Key
+                for i in range(4):
+                    for j in range(4):
+                        statearray[i][j] ^= key_words[(4*(round-1))+i][8 * j:8 + (8 * j)]
+
+                # Inverse ColumnMixing
+                if round != 1:
+                    zeroE = BitVector(bitstring='00001110')
+                    zeroB = BitVector(bitstring='00001011')
+                    zeroD = BitVector(bitstring='00001101')
+                    zero9 = BitVector(bitstring='00001001')
+                    for i in range(4):
+                        temp = (zeroE.gf_multiply_modular(statearray[i][0], AES_modulus, 8)) ^ \
+                               (zeroB.gf_multiply_modular(statearray[i][1], AES_modulus, 8)) ^ \
+                               (zeroD.gf_multiply_modular(statearray[i][2], AES_modulus, 8)) ^ \
+                               (zero9.gf_multiply_modular(statearray[i][3], AES_modulus, 8))
+
+                        temp1 = (zeroE.gf_multiply_modular(statearray[i][1], AES_modulus, 8)) ^ \
+                                (zeroB.gf_multiply_modular(statearray[i][2], AES_modulus, 8)) ^ \
+                                (zeroD.gf_multiply_modular(statearray[i][3], AES_modulus, 8)) ^ \
+                                (zero9.gf_multiply_modular(statearray[i][0], AES_modulus, 8))
+
+                        temp2 = (zeroE.gf_multiply_modular(statearray[i][2], AES_modulus, 8)) ^ \
+                                (zeroB.gf_multiply_modular(statearray[i][3], AES_modulus, 8)) ^ \
+                                (zeroD.gf_multiply_modular(statearray[i][0], AES_modulus, 8)) ^ \
+                                (zero9.gf_multiply_modular(statearray[i][1], AES_modulus, 8))
+
+                        temp3 = (zeroE.gf_multiply_modular(statearray[i][3], AES_modulus, 8)) ^ \
+                                (zeroB.gf_multiply_modular(statearray[i][0], AES_modulus, 8)) ^ \
+                                (zeroD.gf_multiply_modular(statearray[i][1], AES_modulus, 8)) ^ \
+                                (zero9.gf_multiply_modular(statearray[i][2], AES_modulus, 8))
+
+                        statearray[i][0] = temp
+                        statearray[i][1] = temp1
+                        statearray[i][2] = temp2
+                        statearray[i][3] = temp3
+
+            for i in range(4):
+                for j in range(4):
+                    statearray[i][j].write_to_file(FILEOUT)
+                    f.write(statearray[i][j].get_bitvector_in_hex())
+    f.close()
+    FILEOUT.close()
+    return
 
 if __name__ == '__main__':
-    key = get_encryption_key()
-    ok = gen_key_schedule_128(key)
-    print(len(ok))
-    for i in ok:
-        print(i.get_bitv)
+    import os
+    if os.path.isfile('encrypted.txt'):
+        os.remove('encrypted.txt')
+    if os.path.isfile('decrypted.txt'):
+        os.remove('decrypted.txt')
+    if os.path.isfile('hexfile.txt'):
+        os.remove('hexfile.txt')
+    if os.path.isfile('hexdecrypt.txt'):
+        os.remove('hexdecrypt.txt')
+    encrypt('plaintext.txt', 'encrypted.txt')
+    decrypt('encrypted.txt', 'decrypted.txt')
